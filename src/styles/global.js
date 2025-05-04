a63cda79395176deaa50f0cbafefe71f2b9e6783
ceciliore <doandradejr@gmail.com>
2025-05-04 20:05:57 -0300

    Add background global

---
+++ src/styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+    body {
+        background-color: #d2e1f7e8;
+        max-width:100%;
+        margin: 0 auto;
+    }
+    
+`;
